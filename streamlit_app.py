2e50ac07c68fe1ac0bd5c0342b37b7c4926a7d4c
yashjain3745 <yashjain181220@gmail.com>
2024-10-23 12:25:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ if time_to_insert:
  icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
- 
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
